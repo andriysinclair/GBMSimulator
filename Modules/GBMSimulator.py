019af4c676a8481ed1ace4b9affc19889c2d090c
Andriy Sinclair <andriysinclair@outlook.com>
2024-11-07 12:03:07 +0000

    changed the GBMSimulator class a bit, to define parameters within the function'

--- Modules/GBMSimulator.py
+++ Modules/GBMSimulator.py
@@ -2,27 +2,13 @@ import numpy as np
 
 class GBMSimulator:
 
-    def __init__(self, mu,n,T, M, y0, sigma):
-
+    def __init__(self):
+        pass
         '''
-        Initialise parameters for the GBM simulator
-
-        Args:
-        mu (int): drift coefficient
-        n (int): number of steps
-        T (int): time in years
-        M (int): number of sims
-        y0 (int): initial value
-        sigma (int): volatility
+        Initialises Instance of GBMSimulator class
         '''
-        self.mu = mu
-        self.n = n
-        self.T=T
-        self.M = M
-        self.y0 = y0
-        self.sigma = sigma
 
-    def simulate(self):
+    def simulate(self, mu,n,T, M, y0, sigma):
         """
         A function to simulate geometric Brownian motion for given  
 
@@ -45,18 +31,18 @@ class GBMSimulator:
 
         """    
         # calculate each time step
-        dt = self.T/self.n        
+        dt = T/n        
 
         # simulate using numpy array
         st = np.exp(    
-            (self.mu - self.sigma**2 / 2) * dt
-                                    + self.sigma*np.random.normal(0, np.sqrt(dt), size=(self.M,self.n)).T
+            (mu - sigma**2 / 2) * dt
+                                    + sigma*np.random.normal(0, np.sqrt(dt), size=(M,n)).T
                                 )
 
         # include array of 1's
-        st = np.vstack([np.ones(self.M), st])
+        st = np.vstack([np.ones(M), st])
 
-        st = self.y0 * st.cumprod(axis=0)
+        st = y0 * st.cumprod(axis=0)
 
         return st
 
