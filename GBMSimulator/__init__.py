ca010264efc4ac0e4d41c37e861a7a1ff381a1e5
Andriy Sinclair <andriysinclair@outlook.com>
2024-11-08 13:24:05 +0000

    appended __init.py

--- GBMSimulator/__init__.py
+++ GBMSimulator/__init__.py
@@ -1,4 +1,8 @@
 from pathlib import Path
 
+# Inside GBMSimulator/__init__.py
+from .GBMSimulator import GBMSimulator
+'''Ensures that import GBMSimulator works'''
+
 # Absolute path to the modules folder
 modules_path = Path(__file__).parent.parent /  "GBMSimulator"
